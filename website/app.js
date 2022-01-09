1ba60ade281f981a4e031583e9a3dcb8dcd26ee3
Omar <omaar_5@hotmail.com>
2022-01-09 15:06:24 +0200

    setup valid apiKey

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 // Personal API Key for OpenWeatherMap API
+const apiKey = `584befef1f6116016438014a9b30ff2a`;
+console.log(apiKey);
 
 // Event listener to add function to existing HTML DOM element
 
