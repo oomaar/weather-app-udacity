73fa60bb78adc98b9ce55f608c9e5cca3a73e182
Omar <omaar_5@hotmail.com>
2022-01-09 15:04:35 +0200

    spin up the server

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 // Setup empty JS object to act as endpoint for all routes
+let projectData = {};
+
 // Express to run server and routes
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
 /* Middleware*/
-
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static("website"));
+
+// Callback to debug
+// Initialize all route with a callback function
+// Callback function to complete GET '/all'
+// Post Route
+
 
 // Spin up the server
-// Callback to debug
-
-// Initialize all route with a callback function
-
-// Callback function to complete GET '/all'
-
-// Post Route
-  
+const port = `7000`;
+app.listen(port, () => console.log(`Server is up and running on port: http://localhost:${port}`));
