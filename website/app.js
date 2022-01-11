33b0d3d1dea2bfc7238515a1c2084b6bf7d668ef
Omar <omaar_5@hotmail.com>
2022-01-11 22:19:58 +0200

    Step-1: GET Web API Data

--- website/app.js
+++ website/app.js
@@ -5,39 +5,14 @@ const endPoint = `https://api.openweathermap.org/data/2.5/weather`;
 let d = new Date();
 let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
-/* Function to GET Web API Data*/
-
-/* Function to POST data */
-const postData = async data => {
-    await fetch("/add", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            temp: data.main.temp,
-            date: newDate
-        })
-    });
-};
-
-/* Function to GET Project Data */
-const updateUI = async () => {
-    const uiData = await fetch("/all").then(data => data.json());
-    document.getElementById("temp").innerHTML = `${uiData.temp}`;
-    document.getElementById("date").innerText = `${uiData.date}`;
-};
-
 /* Function called by event listener */
 const buttonFunction = async e => {
     e.preventDefault();
-    const feelings = document.getElementById("feelings");
+    // Step-1: GET Web API Data
     const zipCode = document.getElementById("zip");
     const baseURL = `${endPoint}?zip=${zipCode.value}&appid=${apiKey}&units=imperial`;
     const response = await fetch(baseURL).then(data => data.json());
-
-    postData(response);
-    updateUI();
+    console.log("🚀 ~ file: app.js ~ line 37 ~ response", response)
 };
 
 // Event listener to add function to existing HTML DOM element
