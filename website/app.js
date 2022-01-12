f455ba13171fae55f1969925bec54146c1588e95
Omar <omaar_5@hotmail.com>
2022-01-12 15:45:19 +0200

    make changes to meet project rubric

--- website/app.js
+++ website/app.js
@@ -1,13 +1,13 @@
 // Personal API Key for OpenWeatherMap API
-const apiKey = `584befef1f6116016438014a9b30ff2a`;
+const apiKey = `584befef1f6116016438014a9b30ff2a&units=imperial`;
 const endPoint = `https://api.openweathermap.org/data/2.5/weather`;
 
 let d = new Date();
 let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
 /* Function to POST data */
-const postData = async data => {
-    await fetch("/add", {
+const postData = async (url = '', data = {}) => {
+    await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -28,21 +28,26 @@ const postData = async data => {
 
 /* Function to updateUI */
 const updateUI = async () => {
-    const uiData = await fetch("/all").then(data => data.json());
+    const request = await fetch("/all");
     const feelings = document.getElementById("feelings");
 
-    document.getElementById("temp").innerHTML = `${Math.round(uiData.temp)}&deg`;
-    document.getElementById("date").innerHTML = `${uiData.date}`;
-    document.getElementById("users-feeling").innerHTML = `<p>Users Feeling</p> ${feelings.value}`
+    try {
+        const uiData = await request.json();
+        document.getElementById("temp").innerHTML = `${Math.round(uiData.temp)}&deg`;
+        document.getElementById("date").innerHTML = `${uiData.date}`;
+        document.getElementById("users-feeling").innerHTML = `<p>Users Feeling</p> ${feelings.value}`;
 
-    // Extra Data
-    document.getElementById("weather-feelsLiks").innerHTML = `<p>Feels Like</p> ${uiData.feelsLiks} &deg`
-    document.getElementById("humidity").innerHTML = `<p>Humidity</p> ${uiData.humidity} %`
-    document.getElementById("pressure").innerHTML = `<p>Pressure</p> ${uiData.pressure} hPa`
-    document.getElementById("temp-max").innerHTML = `<p>Temp Max</p> ${Math.round(uiData.tempMax)} &deg`
-    document.getElementById("temp-min").innerHTML = `<p>Temp Min</p> ${Math.round(uiData.tempMin)} &deg`
-    document.getElementById("visibility").innerHTML = `<p>Visibility</p> ${uiData.visibility / 1000} km`
-    document.getElementById("wind-speed").innerHTML = `<p>Wind Speed</p> ${uiData.windSpeed} km/h`
+        // Extra Data
+        document.getElementById("weather-feelsLiks").innerHTML = `<p>Feels Like</p> ${uiData.feelsLiks} &deg`;
+        document.getElementById("humidity").innerHTML = `<p>Humidity</p> ${uiData.humidity} %`;
+        document.getElementById("pressure").innerHTML = `<p>Pressure</p> ${uiData.pressure} hPa`;
+        document.getElementById("temp-max").innerHTML = `<p>Temp Max</p> ${Math.round(uiData.tempMax)} &deg`;
+        document.getElementById("temp-min").innerHTML = `<p>Temp Min</p> ${Math.round(uiData.tempMin)} &deg`;
+        document.getElementById("visibility").innerHTML = `<p>Visibility</p> ${uiData.visibility / 1000} km`;
+        document.getElementById("wind-speed").innerHTML = `<p>Wind Speed</p> ${uiData.windSpeed} km/h`;
+    } catch (error) {
+        console.log(error);
+    };
 };
 
 
@@ -52,12 +57,12 @@ const buttonFunction = async e => {
     e.preventDefault();
     // Step-1: GET Web API Data
     const zipCode = document.getElementById("zip");
-    const baseURL = `${endPoint}?zip=${zipCode.value}&appid=${apiKey}&units=imperial`;
+    const baseURL = `${endPoint}?zip=${zipCode.value}&appid=${apiKey}`;
     const response = await fetch(baseURL).then(data => data.json());
     console.log("🚀 ~ file: app.js ~ line 37 ~ response", response);
 
     // Step-2: POST data to Server
-    postData(response);
+    postData("/add", response);
 
     // Step-3: Update UI
     updateUI();
